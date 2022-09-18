9eccbf13c50a3ac292585f7fbbe82c520da2da6b
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-18 22:40:44 +0700

    feat: Find polynomial

--- hw4/hw4-4.py
+++ hw4/hw4-4.py
@@ -9,4 +9,18 @@
 # Пример:
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+from random import randint
+
+def find_polynomial(k):
+    ratios = [str(randint(0, 100)) for i in range(k + 1)]
+    result = ''
+    for i in range(0, k + 1):
+        if i == k:
+            result += ratios[i] + ' = 0'
+        elif i == k - 1:
+            result += ratios[i] + 'x + '
+        elif i < k - 1:
+            result += ratios[i] + 'x^' + str(k - i) + ' + '
+    return result
+
 k = int(input('Введите натуральную степень k = '))
