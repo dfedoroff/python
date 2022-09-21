cf0f8ea88f2d5f438e6afc6e2f2e5ef03ee2b88b
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-21 16:52:59 +0700

    feat: Read data from text file

--- hw5/hw5-1/hw5-1.py
+++ hw5/hw5-1/hw5-1.py
@@ -4,3 +4,8 @@
 # -----------------
 # Задача 1:
 # Напишите программу, удаляющую из текста все слова, содержащие "АБВ".
+
+def read_file(file):
+    with open(file, 'r', encoding='utf-8') as data:
+        raw_text = data.read()
+    return raw_text
