14f60914efe9dd12539ea80baef1451dada65598
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-21 16:55:41 +0700

    dc: Add print method for original data

--- hw5/hw5-1/hw5-1.py
+++ hw5/hw5-1/hw5-1.py
@@ -9,3 +9,5 @@ def read_file(file):
     with open(file, 'r', encoding='utf-8') as data:
         raw_text = data.read()
     return raw_text
+
+print(f'Изначальный текст: {read_file("input-data.txt")}')
