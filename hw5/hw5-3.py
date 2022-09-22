452abbff877bbc25780a7235ea42b3fa98ba4661
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-22 08:27:24 +0700

    feat: Print board in console

--- hw5/hw5-3.py
+++ hw5/hw5-3.py
@@ -6,3 +6,7 @@
 # Создайте программу для игры в "Крестики-нолики".
 
 board = list(range(1, 10))
+
+def print_board(table):
+    for i in range(3):
+        print(table[0 + i * 3], table[1 + i * 3], table[2 + i * 3])
