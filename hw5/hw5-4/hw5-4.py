67ef052db3211310a39022621054475074f532da
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-22 09:26:16 +0700

    feat: Read data from text file

--- hw5/hw5-4/hw5-4.py
+++ hw5/hw5-4/hw5-4.py
@@ -5,3 +5,7 @@
 # Задача 4:
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+def read_file(file):
+    with open(file, 'r') as data:
+        text = data.read()
+    return text
