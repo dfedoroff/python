d1d258e66b7e7f205819ed384a4524a0d053e47a
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-22 08:36:58 +0700

    feat: Get result of game

--- hw5/hw5-3.py
+++ hw5/hw5-3.py
@@ -14,3 +14,13 @@ def print_board(table):
 def make_move(move, symbol):
     i = board.index(move)
     board[i] = symbol
+
+def find_winner():
+    win_coord = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    win = ''
+    for i in win_coord:
+        if (board[i[0]] == '\u001B[34mX\u001b[0m' and board[i[1]]) == ('\u001B[34mX\u001b[0m' and board[i[2]]) == ('\u001B[34mX\u001b[0m'):
+            win = '\u001B[34mигрок 1\u001b[0m'
+        if (board[i[0]] == '\u001B[31mO\u001b[0m' and board[i[1]]) == ('\u001B[31mO\u001b[0m' and board[i[2]]) == ('\u001B[31mO\u001b[0m'):
+            win = '\u001B[31mигрок 2\u001b[0m'
+    return win
