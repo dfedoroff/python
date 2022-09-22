92200db929419a442e05e8cbed94a9f3cbafe2e0
DmitryFedorov <shadowysupercoder@yandex.ru>
2022-09-22 11:41:00 +0700

    feat: Play game main method

--- hw5/hw5-2a.py
+++ hw5/hw5-2a.py
@@ -12,8 +12,43 @@
 # a) Добавьте игру против бота.
 # b) Подумайте как наделить бота "интеллектом".
 
+import random
+
 rules = ('Привет! Это игра "Забери все конфеты!" Ее основные правила: '
             'Участникам дана 2021 конфета, но за один ход можно взять не более 28.')
+
+messages = ['Ваша очередь брать конфеты', 'возьмите конфеты',
+            'сколько конфет возьмёте?', 'берите, не стесняйтесь', 'Ваш ход']
+
+def play_game(n, m, players, messages):
+    count = 0
+    if n % 10 == 1 and 9 > n > 10:
+        letter = 'а'
+    elif 1 < n % 10 < 5 and 9 > n > 10:
+        letter = 'ы'
+    else:
+        letter = ''
+    while n > 0:
+        print(f'{players[count % 2]}, {random.choice(messages)}')
+        move = int(input())
+        if move > n or move > m:
+            print(f'Слишком много! Можно взять не более {m} конфет{letter}, у нас всего {n} конфет{letter}')
+            attempt = 3
+            while attempt > 0:
+                if n >= move <= m:
+                    break
+                print(f'Попробуйте ещё раз, у Вас {attempt} попытки')
+                move = int(input())
+                attempt -= 1
+            else:
+                return print(f'Очень жаль, у Вас не осталось попыток. Game over!')
+        n = n - move
+        if n > 0:
+            print(f'Осталось {n} конфет{letter}')
+        else:
+            print('Все конфеты разобраны.')
+        count += 1
+    return players[not count % 2]
 
 print(rules)
 player1 = input('Представьтесь, пожалуйста: ')
