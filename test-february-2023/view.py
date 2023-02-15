6e6a9ff6e8e99d8b0788349fc24c81e86f25bf4f
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-15 10:55:16 +0700

    feat: Print message that note with entered id already exists

--- test-february-2023/view.py
+++ test-february-2023/view.py
@@ -12,3 +12,7 @@ class View(object):
     @staticmethod
     def print_notes_not_exist():
         print('\nСписок заметок пуст')
+
+    @staticmethod
+    def print_note_id_exist(note_id):
+        print('\nЗаметка с id: {} уже есть'.format(note_id))
