c326dfef8b7cb7df54683946465b1b5f5844c69e
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-15 12:03:25 +0700

    feat: Write JSON file

--- test-february-2023/jsonmodel.py
+++ test-february-2023/jsonmodel.py
@@ -1,4 +1,20 @@
+import json
+
+from view import View
+from note import Note
+
+
 class JSONmodel(object):
     def __init__(self, filename):
         self.filename = filename
         self.notes = list()
+
+    def write_json_file(self, notes):
+        json_strings_list = list()
+        for note in notes:
+            json_strings_list.append({'id': note.note_id, 'date': note.date, 'title': note.title, 'text': note.text})
+
+        notes_json = json.dumps(json_strings_list, indent=4, sort_keys=False, default=str)
+
+        with open(self.filename, "w", encoding='utf-8') as my_file:
+            my_file.write(notes_json)
