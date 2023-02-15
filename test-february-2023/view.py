68ee896ee2e5cedcdb03e06dc7d835fef5ab82eb
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-15 10:51:36 +0700

    feat: Print all saved notes

--- test-february-2023/view.py
+++ test-february-2023/view.py
@@ -3,3 +3,8 @@ class View(object):
     @staticmethod
     def print_saved_note(note):
         print(note)
+
+    @staticmethod
+    def print_all_saved_notes(notes):
+        for note in notes:
+            print(note)
