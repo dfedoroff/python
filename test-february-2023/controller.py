345ab2503fe999d82e7cf7d29c7caf903eeb3622
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-02-15 12:44:00 +0700

    feat: Print all saved notes

--- test-february-2023/controller.py
+++ test-february-2023/controller.py
@@ -3,3 +3,7 @@ class Controller(object):
     def __init__(self, model, view):
         self.model = model
         self.view = view
+
+    def print_all_notes(self):
+        notes = self.model.read_notes()
+        self.view.print_all_saved_notes(notes)
