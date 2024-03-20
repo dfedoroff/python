d0975045d0fb8f1888087111c242f897dd242a2e
DmitryFedorov <shadowysupercoder@yandex.ru>
2024-03-20 09:48:17 +0700

    feat: Add main execution block for demonstration
    #comment Implement main function to demonstrate the transposition and printing of a predefined 3x3 matrix. This serves as the entry point for the program, showcasing the application of transpose_matrix and print_matrix functions
    
    Affected: main

--- data_engineer/hw4/hw4-1.py
+++ data_engineer/hw4/hw4-1.py
@@ -11,3 +11,16 @@ def transpose_matrix(matrix):
 def print_matrix(matrix):
     for row in matrix:
         print("\t".join(str(element) for element in row))
+
+
+def main():
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print("Исходная матрица:")
+    print_matrix(matrix)
+    print("\nТранспонированная матрица:")
+    transposed_matrix = transpose_matrix(matrix)
+    print_matrix(transposed_matrix)
+
+
+if __name__ == "__main__":
+    main()
